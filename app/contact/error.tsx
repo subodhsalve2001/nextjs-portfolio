c3d09ffd3ff50f689c560487594abe8d8eea20b0
Subodh <suboodhsalve2001@gmail.com>
2023-05-25 12:33:32 +0530

    initial commit

---
+++ app/contact/error.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function error() {
+  return (
+    <>
+    <h1>Something went wrong</h1>
+    </>
+  )
+}
+
+export default error
---
+++ app/projects/loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Spinner from '../components/Spinner'
+
+function loading() {
+    return (
+        <>
+            <Spinner />
+        </>
+    )
+}
+
+export default loading
